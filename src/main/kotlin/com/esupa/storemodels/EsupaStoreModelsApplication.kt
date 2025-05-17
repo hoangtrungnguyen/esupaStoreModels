9b0bb084c542a81c666d8b15af0bfe5f4301881b
Anthony Hoang <hoangtrungnguyen18102000@gmail.com>
2025-05-17 11:16:49 +0700

    remove applicatio

--- src/main/kotlin/com/esupa/storemodels/EsupaStoreModelsApplication.kt
+++ src/main/kotlin/com/esupa/storemodels/EsupaStoreModelsApplication.kt
@@ -2,10 +2,3 @@ package com.esupa.storemodels
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-
-@SpringBootApplication
-class EsupaStoreModelsApplication
-
-fun main(args: Array<String>) {
-    runApplication<EsupaStoreModelsApplication>(*args)
-}
